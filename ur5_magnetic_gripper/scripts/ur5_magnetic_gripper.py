50f1703259002cbb45ec9d7e29c6c739d313a2e9
jgoncalves <joaocabogoncalves@tecnico.ulisboa.pt>
2019-06-24 18:19:21 +0100

    fixed bugs ur5_magnetic_gripper

--- ur5_magnetic_gripper/scripts/ur5_magnetic_gripper.py
+++ ur5_magnetic_gripper/scripts/ur5_magnetic_gripper.py
@@ -9,14 +9,14 @@ import PyKDL #Vector, Rotation, Quaternion, Frame
 
 from mbzirc_comm_objs.srv import Magnetize, MagnetizeResponse
 from mbzirc_comm_objs.msg import GripperAttached
-from IOService.srv import setIO, setIORequest
+from ur_msgs.srv import SetIO, SetIORequest
 from geometry_msgs.msg import WrenchStamped
 
 class node():
 
     def magnetize_cb(self,req):
-        v = 24 if req.magnetize else 0
-        self.set_io(fun=setIORequest.FUN_SET_TOOL_VOLTAGE,state=v)
+        v = 0 if req.magnetize else 24
+        self.set_io(fun=SetIORequest.FUN_SET_TOOL_VOLTAGE,state=v)
         return MagnetizeResponse(success=True)
 
     def wrench_cb(self, msg):
@@ -24,17 +24,17 @@ class node():
         if abs(msg.wrench.force.y) > self.threshold and not self.attached:
             self.attached = True
             self.pub.publish(attached=True)
-        elif abs(msg.wrench.force.y) < self.threshold and self.attached
+        elif abs(msg.wrench.force.y) < self.threshold and self.attached:
             self.attached = False
             self.pub.publish(attached=False)
 
     def __init__(self):
 
-        self.set_io = rospy.ServiceProxy('ur_driver/set_io', setIO)
-        rospy.Service('magnetize', Empty, self.magnetize_cb)
+        self.set_io = rospy.ServiceProxy('ur_driver/set_io', SetIO)
+        rospy.Service('magnetize', Magnetize, self.magnetize_cb)
 
         self.pub = rospy.Publisher('attached', GripperAttached, queue_size=1)
-        self.sub = rospy.Subscriber('/wrench', WrenchStamped, wrench_cb)
+        self.sub = rospy.Subscriber('/wrench', WrenchStamped, self.wrench_cb)
 
         self.attached = False
         self.threshold = 15 #N
