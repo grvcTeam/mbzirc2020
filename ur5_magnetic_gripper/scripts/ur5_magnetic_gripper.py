cdb6895515e164accfc245b2c20eb5606ed6fe23
jgoncalves <joaocabogoncalves@tecnico.ulisboa.pt>
2019-06-24 18:31:58 +0100

    using URScript publisher to magnetize

--- ur5_magnetic_gripper/scripts/ur5_magnetic_gripper.py
+++ ur5_magnetic_gripper/scripts/ur5_magnetic_gripper.py
@@ -11,12 +11,14 @@ from mbzirc_comm_objs.srv import Magnetize, MagnetizeResponse
 from mbzirc_comm_objs.msg import GripperAttached
 from ur_msgs.srv import SetIO, SetIORequest
 from geometry_msgs.msg import WrenchStamped
+from std_msgs.msg import String
 
 class node():
 
     def magnetize_cb(self,req):
         v = 0 if req.magnetize else 24
-        self.set_io(fun=SetIORequest.FUN_SET_TOOL_VOLTAGE,state=v)
+        #self.set_io(fun=SetIORequest.FUN_SET_TOOL_VOLTAGE,state=v)
+        self.pub2.publish(data='set_tool_voltage({v})'.format(v=v))
         return MagnetizeResponse(success=True)
 
     def wrench_cb(self, msg):
@@ -30,10 +32,11 @@ class node():
 
     def __init__(self):
 
-        self.set_io = rospy.ServiceProxy('ur_driver/set_io', SetIO)
+        #self.set_io = rospy.ServiceProxy('ur_driver/set_io', SetIO)
         rospy.Service('magnetize', Magnetize, self.magnetize_cb)
 
         self.pub = rospy.Publisher('attached', GripperAttached, queue_size=1)
+        self.pub2 = rospy.Publisher('/ur_driver/URScript', String, queue_size=1)
         self.sub = rospy.Subscriber('/wrench', WrenchStamped, self.wrench_cb)
 
         self.attached = False
