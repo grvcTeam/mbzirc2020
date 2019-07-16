595eb2f43915155e091a8aea0b69470a00617dfa
Fran Real <fran.real.perez@gmail.com>
2019-07-16 12:53:40 +0200

    Test vanilla particle flilter

--- mc_localization/src/mcl_node.cpp
+++ mc_localization/src/mcl_node.cpp
@@ -1,4 +1,5 @@
 #include <random>
+#include <algorithm>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <nav_msgs/OccupancyGrid.h>
@@ -13,7 +14,6 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-// TODO: Use geometry_msgs::Pose?
 struct Pose2D {
     double x;
     double y;
@@ -21,19 +21,10 @@ struct Pose2D {
 };
 
 struct Noise {
-    std::normal_distribution<double> move  = std::normal_distribution<double>(0, 0);
-    std::normal_distribution<double> turn  = std::normal_distribution<double>(0, 0);
-    std::normal_distribution<double> sense = std::normal_distribution<double>(0, 0);
+    std::normal_distribution<double> move  = std::normal_distribution<double>(0, 0.2);
+    std::normal_distribution<double> turn  = std::normal_distribution<double>(0, 0.2);
+    std::normal_distribution<double> sense = std::normal_distribution<double>(0, 2.0);
 };
-
-// TODO: Use OccupancyGrid
-struct Map {
-    double width;
-    double height;
-};
-
-// TODO: Use part of LaserScan?
-// struct ScanParams {};
 
 // TODO: Use doubles or floats?
 inline double normal_pdf(double mu, double sigma, double x) {
@@ -48,14 +39,19 @@ inline double normalizeAngle(double angle) {
     return angle;
 }
 
-inline bool poseIsInMap(const Pose2D& _pose, const Map& _map) {
-    if (_pose.x < 0 || _pose.x >= _map.width) {
-        // TODO: Values < 0 are possible?
+inline bool poseIsInMap(const Pose2D& _pose, const nav_msgs::MapMetaData& _map_info) {
+    // TODO: Calculate this each time is not efficient!
+    double x_min = _map_info.origin.position.x;
+    double x_max = x_min + _map_info.width * _map_info.resolution;
+    double y_min = _map_info.origin.position.y;
+    double y_max = y_min + _map_info.height * _map_info.resolution;
+    ROS_INFO("map: [%lf, %lf] x [%lf, %lf]", x_min, x_max, y_min, y_max);
+
+    if (_pose.x < x_min || _pose.x >= x_max) {
         ROS_ERROR("poseIsInMap: %lf out of x bounds", _pose.x);
         return false;
     }
-    if (_pose.y < 0 || _pose.y >= _map.height) {
-        // TODO: Values < 0 are possible?
+    if (_pose.y < y_min || _pose.y >= y_max) {
         ROS_ERROR("poseIsInMap: %lf out of y bounds", _pose.y);
         return false;
     }
@@ -67,12 +63,23 @@ public:
 
     RayCast() = default;
 
-    void setScanParams(double _angle_min, double _angle_max, double _angle_inc, double _range_min, double _range_max) {
-        angle_min_ = _angle_min;
-        angle_max_ = _angle_max;
-        angle_inc_ = _angle_inc;
-        range_min_ = _range_min;
-        range_max_ = _range_max;
+    void setScanParams(const sensor_msgs::LaserScan& _scan_params) {
+        angle_min_ = _scan_params.angle_min;
+        angle_max_ = _scan_params.angle_max;
+        angle_inc_ = _scan_params.angle_increment;
+        range_min_ = _scan_params.range_min;
+        range_max_ = _scan_params.range_max;
+    }
+
+    sensor_msgs::LaserScan getScanParams() {
+        sensor_msgs::LaserScan scan_params;
+        scan_params.angle_min       = angle_min_;
+        scan_params.angle_max       = angle_max_;
+        scan_params.angle_increment = angle_inc_;
+        scan_params.range_min       = range_min_;
+        scan_params.range_max       = range_max_;
+
+        return scan_params;
     }
 
     void setGrid(cv::Mat& _grid, double _m_per_px, cv::Point _offset) {
@@ -81,17 +88,10 @@ public:
         grid_offset_ = _offset;
     }
 
-    sensor_msgs::LaserScan scan(Pose2D _laser_pose) {
-
+    std::vector<float> scan(Pose2D _laser_pose) {
+        std::vector<float> ranges;
         cv::Point2f start((_laser_pose.x - grid_offset_.x) / m_per_px_, (_laser_pose.y - grid_offset_.y) / m_per_px_);
         double yaw = _laser_pose.yaw;
-
-        sensor_msgs::LaserScan scan;
-        scan.angle_min = angle_min_;
-        scan.angle_max = angle_max_;
-        scan.angle_increment = angle_inc_;
-        scan.range_min = range_min_;
-        scan.range_max = range_max_;
 
         cv::Point2f hit;
         double max_px = range_max_ / m_per_px_;
@@ -103,12 +103,12 @@ public:
                 if (range < range_min_) {
                     range = range_max_ + 1.0;  // Out of range (range > range_max)
                 }
-                scan.ranges.push_back(range);
+                ranges.push_back(range);  // TODO: Add noise here?
             } else {
-                scan.ranges.push_back(range_max_ + 1.0);
+                ranges.push_back(range_max_ + 1.0);
             }
         }
-        return scan;
+        return ranges;
     }
 
 protected:
@@ -144,24 +144,16 @@ protected:
 class LaserSim {
 public:
 
-    LaserSim(ros::NodeHandle node) {
-
+    LaserSim() {
         ray_cast_ = std::make_shared<RayCast>();
-        ray_cast_->setScanParams(node.param<double>("angle/min", -M_PI_2),
-                                 node.param<double>("angle/max", M_PI_2),
-                                 node.param<double>("angle/increment", 0.01),
-                                 node.param<double>("range/min", 1.0),
-                                 node.param<double>("range/max", 20.0)
-                                 );
-
         // Subscribe / Publish
+        ros::NodeHandle node;
         map_sub_ = node.subscribe(map_topic_, 1, &LaserSim::mapCallback, this);
         laser_pub_ = node.advertise<sensor_msgs::LaserScan>(laser_topic_, 1);
     }
 
     void mapCallback(const nav_msgs::OccupancyGrid::Ptr& grid) {
-
-        ROS_INFO("Updating map...");
+        ROS_INFO("LaserSim: Updating map...");
         map_ = *grid;
 
         cv::Mat map_mat = cv::Mat(map_.info.height, map_.info.width, CV_8UC1, map_.data.data());  // Convert OccupancyGrid to cv::Mat, uint8_t
@@ -170,10 +162,9 @@ public:
         map_loaded_ = true;
     }
 
-    void update(Pose2D _laser_pose)  {
+    void publish_sim(const Pose2D& _laser_pose) {
         if (!map_loaded_) {
             ROS_WARN("LaserSim: Update called, no map yet");
-            return;
         }
 
         static tf2_ros::TransformBroadcaster tf_broadcaster;
@@ -193,7 +184,8 @@ public:
         tf_laser.transform.rotation.w = q.w();
         tf_broadcaster.sendTransform(tf_laser);
 
-        sensor_msgs::LaserScan scan = ray_cast_->scan(_laser_pose);
+        sensor_msgs::LaserScan scan = ray_cast_->getScanParams();
+        scan.ranges = ray_cast_->scan(_laser_pose);
         scan.header.stamp = ros::Time::now();
         scan.header.frame_id = "laser";  // TODO: laser_frame_id
         laser_pub_.publish(scan);
@@ -215,32 +207,91 @@ protected:
 // TODO: Wouldn't be faster to just do one conversion, from real measurement in meters to pixels,
 // instead of many conversions from pixel to meters (enhancement)
 
-class RobotParticle {
+/*
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(1, 2);
+    for (int n = 0; n < 10; ++n) {
+        std::cout << dis(gen) << ' ';
+    }
+    std::cout << '\n';
+ */
+/*
+    std::random_device rd;
+    std::mt19937 gen(rd());
+ 
+    // values near the mean are the most likely
+    // standard deviation affects the dispersion of generated values from the mean
+    std::normal_distribution<> d(5,2);
+ 
+    std::map<int, int> hist;
+    for(int n=0; n<10000; ++n) {
+        ++hist[std::round(d(gen))];
+    }
+    for(auto p : hist) {
+        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+    }
+ */
+
+class ParticleSet {
 public:
-    Map map_;
-    Pose2D pose_;
-    Noise noise_;
+    std::vector<Pose2D> poses_;
+    std::vector<double> weights_;
+
+    nav_msgs::OccupancyGrid map_;
     sensor_msgs::LaserScan scan_;
+    bool map_loaded_ = false;
+    bool scan_updated_ = false;
+    std::shared_ptr<RayCast> ray_cast_;
+
+    Noise noise_;
     std::default_random_engine random_engine_;
+    // std::random_device rd;
+    // std::mt19937 gen(rd());
 
-    RobotParticle() = default;
+    // ros::Publisher laser_pub_;
+    ros::Subscriber map_sub_;
+    ros::Subscriber laser_sub_;
 
-    void setMap(const Map& _map) {
-        map_ = _map;
+    std::string map_topic_ = "/map";
+    // std::string laser_topic_ = "/scan";
+    std::string laser_sub_topic_ = "/scan";
+
+    ParticleSet() {
+
+        ray_cast_ = std::make_shared<RayCast>();
+
+        // Subscribe / Publish
+        ros::NodeHandle n;
+        // laser_pub_ = n.advertise<sensor_msgs::LaserScan>(laser_topic_, 1);
+        map_sub_ = n.subscribe(map_topic_, 1, &ParticleSet::mapCallback, this);
+        laser_sub_ = n.subscribe(laser_sub_topic_, 1, &ParticleSet::laserCallback, this);
     }
 
-    void setPose(const Pose2D& _pose) {
-        if (!poseIsInMap(_pose, map_)) {
-            ROS_ERROR("setPose: pose is not in map!");
-            return;
+    void init(size_t _particle_count, std::uniform_real_distribution<double>& _x, std::uniform_real_distribution<double>& _y, std::uniform_real_distribution<double>& _yaw) {
+        poses_.resize(_particle_count);
+        weights_.resize(_particle_count);
+        for (size_t i = 0; i < poses_.size(); i++) {
+            poses_[i].x   = _x(random_engine_);
+            poses_[i].y   = _y(random_engine_);
+            poses_[i].yaw = _yaw(random_engine_);
         }
+    }
 
-        pose_ = _pose;
+    void mapCallback(const nav_msgs::OccupancyGrid::Ptr& msg) {
+        ROS_INFO("ParticleSet: Updating map...");
+        map_ = *msg;
 
-        if (pose_.yaw < -M_PI || pose_.yaw > M_PI) {
-            ROS_WARN("setPose: %lf out of yaw bounds, normalizing...", pose_.yaw);
-            pose_.yaw = normalizeAngle(pose_.yaw);
-        }
+        cv::Mat map_mat = cv::Mat(map_.info.height, map_.info.width, CV_8UC1, map_.data.data());  // Convert OccupancyGrid to cv::Mat, uint8_t
+        cv::threshold(map_mat, map_mat, 254, 255, cv::THRESH_TOZERO_INV);  // Set unknown space (255) to free space (0)
+        ray_cast_->setGrid(map_mat, map_.info.resolution, cv::Point(map_.info.origin.position.x, map_.info.origin.position.y));  // Update map
+        map_loaded_ = true;
+    }
+
+    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+        scan_ = *msg;
+        scan_updated_ = true;
     }
 
     void setNoise(const Noise& _noise) {
@@ -248,129 +299,137 @@ public:
         noise_ = _noise;
     }
 
-    void setMeasurement(const sensor_msgs::LaserScan& _scan) {
-        // TODO: this goes in a callback
-        scan_ = _scan;
-    }
+    // void setPose(const Pose2D& _pose) {
+    //     if (!poseIsInMap(_pose, map_)) {
+    //         ROS_ERROR("setPose: pose is not in map!");
+    //         return;
+    //     }
 
-    void sense() {
-        // TODO: Here goes ray_tracing
-        // Z = []
-        // for (size_t i = 0; i < scan_.lenght; i++) {
-        //     dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
-        //     dist += random.gauss(0.0, self.sense_noise)
-        //     Z.append(dist)
-        // }
-        // return Z
-    }
-    
+    //     pose_ = _pose;
+
+    //     if (pose_.yaw < -M_PI || pose_.yaw > M_PI) {
+    //         ROS_WARN("setPose: %lf out of yaw bounds, normalizing...", pose_.yaw);
+    //         pose_.yaw = normalizeAngle(pose_.yaw);
+    //     }
+    // }
+
     void move(const Pose2D& _delta_pose) {
-        
-        // Turn, and add randomness to the turning command
-        pose_.yaw += _delta_pose.yaw + noise_.turn(random_engine_);
-        pose_.yaw = normalizeAngle(pose_.yaw);
+        // TODO: Make noise for all particles equal?
+        for (size_t i = 0; i < poses_.size(); i++) {
+            Pose2D new_pose = poses_[i];
+            // Turn, and add randomness to the turning command
+            new_pose.yaw += _delta_pose.yaw + noise_.turn(random_engine_);
+            new_pose.yaw = normalizeAngle(new_pose.yaw);
 
-        // Move, and add randomness to the motion command
-        pose_.x += _delta_pose.x + noise_.move(random_engine_);  // TODO: noise is added twice!
-        pose_.y += _delta_pose.y + noise_.move(random_engine_);  // TODO: noise is added twice!
-        if (!poseIsInMap(pose_, map_)) {
-            ROS_ERROR("move: pose is not in map!");
-            // TODO: cyclic truncate?
+            // Move, and add randomness to the motion command
+            new_pose.x += _delta_pose.x + noise_.move(random_engine_);  // TODO: noise is added twice!
+            new_pose.y += _delta_pose.y + noise_.move(random_engine_);  // TODO: noise is added twice!
+            if (poseIsInMap(new_pose, map_.info)) {
+                poses_[i] = new_pose;
+            }
+        }
+    }
+
+    void weigh() {
+        if (!scan_updated_) { 
+            ROS_ERROR("weigh: scan not updated!"); 
             return;
         }
-    }
-    
-    double weight() {        
-        double prob = 1.0;
-        double sense_std_dev = noise_.sense.stddev();
-        for (size_t i = 0; i < scan_.ranges.size(); i++) {
-            double dist = 0;  // TODO: result from sense() function
-            prob *= normal_pdf(dist, sense_std_dev, scan_.ranges[i]);
+        if (!map_loaded_) { 
+            ROS_ERROR("weigh: map not loaded!"); 
+            return;
         }
-        return prob;
+
+        ray_cast_->setScanParams(scan_);
+        double sense_std_dev = noise_.sense.stddev();
+        printf("sense_std_dev = %lf\n", sense_std_dev);
+        for (size_t i = 0; i < poses_.size(); i++) {
+            std::vector<float> sensed = ray_cast_->scan(poses_[i]);  // TODO: there is no sense_noise here?
+            if (scan_.ranges.size() != sensed.size()) {
+                ROS_ERROR("Scan size (%ld) differs from sensed size (%ld)", scan_.ranges.size(), sensed.size());
+            }
+            // TODO: Use OpenCV to compare scans?
+            double w = 1.0;
+            for (size_t j = 0; j < scan_.ranges.size(); j++) {
+                // printf("normal_pdf(sensed[j], sense_std_dev, scan_.ranges[j]) = %lf\n", normal_pdf(sensed[j], sense_std_dev, scan_.ranges[j]));
+                printf("w at iteration [%ld] = %lf\n", j, w);
+                w *= normal_pdf(sensed[j], sense_std_dev, scan_.ranges[j]);
+            }
+            weights_[i] = w;
+            printf("weight[%ld] = %lf\n", i, w);
+        }
     }
 
+    void resample() {
+        int n = poses_.size();
+        if (n == 0) {
+            ROS_ERROR("Trying to resample an empty set!");
+            return;
+        }
+
+        std::uniform_int_distribution<int> uniform_int(0, n);
+        int index = uniform_int(random_engine_);
+        double beta = 0;
+        double max_weight = *std::max_element(weights_.begin(), weights_.end());
+        ROS_INFO("Max weight = %lf", max_weight);
+        std::uniform_real_distribution<double> uniform_real(0.0, 2.0 * max_weight);
+
+        std::vector<Pose2D> new_set(n);
+        for (int i = 0; i < n; i++) {
+            beta += uniform_real(random_engine_);
+            while (beta > weights_[index]) {
+                beta -= weights_[index];
+                index = (index + 1) % n;
+            }
+            new_set[i] = poses_[index];
+        }
+        poses_ = new_set;
+    }
+    // TODO: move + weigh + resample = update (only public interface!) (all checks: n, map, scan)
+
+    void print() {
+        if (!scan_updated_) { 
+            ROS_ERROR("print: scan not updated!"); 
+            return;
+        }
+        if (!map_loaded_) { 
+            ROS_ERROR("print: map not loaded!"); 
+            return;
+        }
+
+        for(size_t i = 0; i < poses_.size(); i++) {
+            ROS_INFO("[%ld] in [%lf, %lf, %lf] with weight [%lf]", i, poses_[i].x, poses_[i].y, poses_[i].yaw, weights_[i]);
+        }
+    }
 };
 
-/*
-// struct LaserConfig {
-
-//     LaserConfig() {}
-//     LaserConfig(const sensor_msgs::LaserScan& _scan) {
-//         angle_min = _scan.angle_min;
-//         angle_max = _scan.angle_max;
-//         angle_increment = _scan.angle_increment;
-//         time_increment = _scan.time_increment;
-//         scan_time = _scan.scan_time;
-//         range_min = _scan.range_min;
-//         range_max = _scan.range_max;
-//     }
-
-//     double angle_min;        // start angle of the scan [rad]
-//     double angle_max;        // end angle of the scan [rad]
-//     double angle_increment;  // angular distance between measurements [rad]
-//     double time_increment;   // time between measurements [seconds] - if your scanner
-//                             // is moving, this will be used in interpolating position
-//                             // of 3d points
-//     double scan_time;        // time between scans [seconds]
-//     double range_min;        // minimum range value [m]
-//     double range_max;        // maximum range value [m]
-// };
-
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-  ROS_INFO("laserCallback!");
-}
-
-sensor_msgs::LaserScan scan(nav_msgs::OccupancyGrid _map, geometry_msgs::Pose _pose, sensor_msgs::LaserScan _laser_config) {
-    sensor_msgs::LaserScan output = _laser_config;  // TODO: copy just config!
-    return output;
-}
-
-
-std::vector<sensor_msgs::LaserScan> update(const std::vector<geometry_msgs::Pose>& _poses) {
-    std::vector<sensor_msgs::LaserScan> output;
-    for (auto pose: _poses) {
-        output.push_back(scan(map, pose, laser_config));
-    }
-    return output;
-}
-*/
 
 int main(int _argc, char** _argv) {
 
     ros::init(_argc, _argv, "mcl_node");
-    ros::NodeHandle n;
 
-    // ros::Subscriber laser_sub = n.subscribe("laser", 1, laserCallback);
-    // RobotParticle robot;
-    LaserSim laser(n);
+    LaserSim laser;
+    ParticleSet particle_set;
 
     Pose2D pose;
     pose.x = 1;
     pose.y = 2;
     pose.yaw = 0.5;
 
-    ros::Rate rate(10);  // [Hz]
+    std::uniform_real_distribution<double> x_position(-10.0, 10.0);
+    std::uniform_real_distribution<double> y_position(-10.0, 10.0);
+    std::uniform_real_distribution<double> yaw_angle(-M_PI, M_PI);
+    particle_set.init(10, x_position, y_position, yaw_angle);
+
+    ros::Rate rate(0.5);  // [Hz]
     while(ros::ok()) {
         ros::spinOnce();
-        laser.update(pose);
+        laser.publish_sim(pose);
+        particle_set.weigh();
+        particle_set.resample();
+        particle_set.print();
         rate.sleep();
     }
-    
-    // tf2::Transform map_to_image;
-    // map_to_image.setOrigin(tf2::Vector3(-30, -10, 0));
-    // map_to_image.setRotation(tf2::Quaternion(0, 0, 0, 1));
-    // std::cout << tf2::toMsg(map_to_image) << '\n';
-
-    // tf2::Transform map_to_laser;
-    // map_to_laser.setOrigin(tf2::Vector3(15, 5, 0));
-    // map_to_laser.setRotation(tf2::Quaternion(0, 0, 0.383, 0.924));
-    // std::cout << tf2::toMsg(map_to_laser) << '\n';
-
-    // tf2::Transform image_to_laser = map_to_image.inverseTimes(map_to_laser);
-    // std::cout << tf2::toMsg(image_to_laser) << '\n';
-
-    ros::spin();
 
     return 0;
 }
