8de262d0a3743faab9e858fd7487213d3e846d76
Fran Real <fran.real.perez@gmail.com>
2019-07-29 16:38:24 +0200

    Get owner id back to region management

--- shared_regions_manager/scripts/shared_regions_manager.py
+++ shared_regions_manager/scripts/shared_regions_manager.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
-from mbzirc_comm_objs.srv import AddSharedRegion, AddSharedRegionResponse, RemoveSharedRegion, RemoveSharedRegionResponse
+from mbzirc_comm_objs.srv import AddSharedRegion, AddSharedRegionResponse
+# from std_msgs.msg import ColorRGBA
 from geometry_msgs.msg import PointStamped, Pose, Vector3
 from visualization_msgs.msg import Marker, MarkerArray
 
@@ -21,19 +22,18 @@ class Interval(object):
         return abs(self.max_value - self.min_value)
 
 class Region(object):
-    def __init__(self, min_corner, max_corner): #, waiting_points):
+    def __init__(self, min_corner, max_corner):
         self.frame_id = 'map'
-        # TODO: check frame_id consistency and transform if needed (all points should be in a common frame_id)
+
+        if min_corner.header.frame_id != self.frame_id:
+            raise ValueError('frame_id = {} not expected'.format(min_corner.header.frame_id))  # TODO: transform?
+            
+        if max_corner.header.frame_id != self.frame_id:
+            raise ValueError('frame_id = {} not expected'.format(max_corner.header.frame_id))  # TODO: transform?
+
         self.x_interval = Interval(min_corner.point.x, max_corner.point.x)
         self.y_interval = Interval(min_corner.point.y, max_corner.point.y)
         self.z_interval = Interval(min_corner.point.z, max_corner.point.z)
-
-        # for point in waiting_points:
-        #     # TODO: check frame_id consisntency
-        #     # TODO: check point is inside region
-        #     print(point)
-        #     pass
-        # self.waiting_points =  waiting_points
 
     def overlaps_with(self, other):
         return self.x_interval.overlaps_with(other.x_interval) and self.y_interval.overlaps_with(other.y_interval) and self.z_interval.overlaps_with(other.z_interval)
@@ -56,50 +56,39 @@ class Region(object):
 class SharedRegionsManager():
 
     def __init__(self):
-        self.id_count = 0
         self.regions = {}
         self.marker_duration = rospy.Duration(1.0)
         rospy.Service('add_shared_region', AddSharedRegion, self.add_region_callback)
-        rospy.Service('remove_shared_region', RemoveSharedRegion, self.remove_region_callback)
         self.marker_array_pub = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size = 1)  # TODO: namespacing?
         rospy.Timer(self.marker_duration, self.publish_marker_callback)
 
     def add_region_callback(self, req):
         proposed_region = Region(req.min_corner, req.max_corner)
-        for region_id, region in self.regions.items():
-            if region.overlaps_with(proposed_region):
-                rospy.logwarn("Proposed region overlaps with region {}".format(region_id))
-                return AddSharedRegionResponse(success = False, region_id = -1)
-        self.id_count += 1
-        self.regions[self.id_count] = proposed_region
-        return AddSharedRegionResponse(success = True, region_id = self.id_count)
-
-    def remove_region_callback(self, req):
-        success = False
-        if req.region_id in self.regions:
-            del self.regions[req.region_id]
-            success = True
-        return RemoveSharedRegionResponse(success = success)
+        for owner_id, region in self.regions.items():
+            if region.overlaps_with(proposed_region) and owner_id != req.agent_id:
+                rospy.logwarn("Proposed region by agent {} overlaps with agent {}".format(req.agent_id, owner_id))
+                return AddSharedRegionResponse(success = False)
+        self.regions[req.agent_id] = proposed_region
+        return AddSharedRegionResponse(success = True)
 
     def publish_marker_callback(self, event):
         marker_array = MarkerArray()
-        for region_id, region in self.regions.items():
+        for owner_id, region in self.regions.items():
             id_marker = Marker()
             id_marker.header.frame_id = region.frame_id
             id_marker.header.stamp = rospy.Time.now()
-            id_marker.ns = 'region_id'
+            id_marker.ns = 'region_owner'
             id_marker.lifetime = self.marker_duration
-            id_marker.id = region_id
+            id_marker.id = owner_id
             id_marker.type = Marker.TEXT_VIEW_FACING
             id_marker.action = Marker.ADD
             id_marker.pose = region.get_pose()
-            id_marker.pose.position.z += 0.15  # A little bit up
-            id_marker.scale.z = 0.1
+            id_marker.scale.z = 0.5
             id_marker.color.r = 1.0
             id_marker.color.g = 1.0
             id_marker.color.b = 1.0
             id_marker.color.a = 1.0
-            id_marker.text = str(region_id)
+            id_marker.text = str(owner_id)
             marker_array.markers.append(id_marker)
 
             region_marker = Marker()
@@ -107,14 +96,14 @@ class SharedRegionsManager():
             region_marker.header.stamp = rospy.Time.now()
             region_marker.ns = 'region_dimensions'
             region_marker.lifetime = self.marker_duration
-            region_marker.id = region_id
+            region_marker.id = owner_id
             region_marker.type = Marker.CUBE
             region_marker.action = Marker.ADD
             region_marker.pose = region.get_pose()
             region_marker.scale = region.get_scale()
-            region_marker.color.r = 1.0  # TODO: color from owner_id
-            region_marker.color.g = 0.0
-            region_marker.color.b = 0.0
+            region_marker.color.r = 0.8  # TODO: color from owner_id
+            region_marker.color.g = 0.2
+            region_marker.color.b = 0.2
             region_marker.color.a = 0.5
             marker_array.markers.append(region_marker)
 
--- shared_regions_manager/scripts/test.py
+++ shared_regions_manager/scripts/test.py
@@ -1,43 +1,41 @@
 #!/usr/bin/env python
 import rospy
-from mbzirc_comm_objs.srv import AddSharedRegion, AddSharedRegionRequest, RemoveSharedRegion, RemoveSharedRegionRequest
+from mbzirc_comm_objs.srv import AddSharedRegion, AddSharedRegionRequest
 from geometry_msgs.msg import PointStamped
+import random
 import time
 
 if __name__ == '__main__':
     rospy.init_node('shared_regions_manager_test', anonymous = True)
 
     add_shared_region_url = 'add_shared_region'
-    remove_shared_region_url = 'remove_shared_region'
-
     rospy.wait_for_service(add_shared_region_url)
-    rospy.wait_for_service(remove_shared_region_url)
-
     add_shared_regions = rospy.ServiceProxy(add_shared_region_url, AddSharedRegion)
-    remove_shared_regions = rospy.ServiceProxy(remove_shared_region_url, RemoveSharedRegion)
 
+    count = 0
+    max_count = 5
     while not rospy.is_shutdown():
 
+        x = random.random() * 3.0
+        y = random.random() * 3.0
+        z = random.random() * 3.0
+
         add_request = AddSharedRegionRequest()
+        add_request.agent_id = count
         add_request.min_corner = PointStamped()
         add_request.min_corner.header.frame_id = 'map'
-        add_request.min_corner.point.x = 0
-        add_request.min_corner.point.y = 0
-        add_request.min_corner.point.z = 0
+        add_request.min_corner.point.x = x
+        add_request.min_corner.point.y = y
+        add_request.min_corner.point.z = z
         add_request.max_corner = PointStamped()
         add_request.max_corner.header.frame_id = 'map'
-        add_request.max_corner.point.x = 1
-        add_request.max_corner.point.y = 2
-        add_request.max_corner.point.z = 3
+        add_request.max_corner.point.x = x + 1.0
+        add_request.max_corner.point.y = y + 1.0
+        add_request.max_corner.point.z = z + 1.0
 
+        print(add_request)
         add_response = add_shared_regions(add_request)
         print(add_response)
+
         time.sleep(3)
-
-        if (add_response.success):
-            remove_request = RemoveSharedRegionRequest()
-            remove_request.region_id = add_response.region_id
-            remove_response = remove_shared_regions(remove_request)
-            time.sleep(3)
-
-    rospy.spin()
+        count = (count + 1) % max_count
