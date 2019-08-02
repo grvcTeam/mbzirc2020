fbaf2fe586a8d1955d75d8a71b9631c68d623e8d
Fran Real <fran.real.perez@gmail.com>
2019-08-02 11:09:38 +0200

    Test function to generate build wall sequence

---
+++ agent_nodes/scripts/test.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+from geometry_msgs.msg import Vector3, Point, PoseStamped
+
+
+# PARAMETER	SPECIFICATION
+# Number of UAVs per team	Maximum of 3
+# Number of UGVs per team	1
+# Arena size	50mx60mx20m
+# Brick shapes and material	Rectangular cube, Styrofoam material
+# Bricks size (Red, Green, Blue)	Approximately 0.30mx0.20mx0.20m, 0.60mx0.20mx0.20m and1.20x0.20x0.20m
+# Bricks size (Orange)	1.80x0.20x0.20 m
+# Weight of bricks	O <= 2.0kg , B <= 1.5kg , G <= 1kg , R <= 1kg,
+# Brick gripping mechanism	Primarily magnetic, but other gripping mechanisms could be used
+# Environment	Outdoor
+# Mode of operation	Autonomous; manual allowed but penalized
+# RTK/DGPS	Allowed but penalized
+# Challenge duration	30 minutes
+# Communications	TBD
+
+# TODO: use enums instead of strings
+brick_scales = {}
+brick_scales['red'] = Vector3(x = 0.3, y = 0.2, z = 0.2)  # TODO: from config file?
+brick_scales['green'] = Vector3(x = 0.6, y = 0.2, z = 0.2)  # TODO: from config file?
+brick_scales['blue'] = Vector3(x = 1.2, y = 0.2, z = 0.2)  # TODO: from config file?
+brick_scales['orange'] = Vector3(x = 1.8, y = 0.2, z = 0.2)  # TODO: from config file?
+
+# TODO: from especification, assume x-z layout
+wall_blueprint = [['red', 'green', 'blue', 'orange'], ['orange', 'blue', 'green', 'red']]
+
+class BrickInWall(object):
+    def __init__(self, color, position):
+        self.color = color
+        self.pose = PoseStamped()
+        self.pose.header.frame_id = 'wall'  # TODO: define this frame
+        self.pose.pose.position = position
+        self.pose.pose.orientation.w = 1  # Assume wall is x-oriented
+
+    def __repr__(self):
+        return '[color = {}, pose = [{}: ({},{},{}) ({},{},{},{})]]'.format(self.color, self.pose.header.frame_id, 
+                self.pose.pose.position.x, self.pose.pose.position.y, self.pose.pose.position.z, 
+                self.pose.pose.orientation.x, self.pose.pose.orientation.y, self.pose.pose.orientation.z, self.pose.pose.orientation.w)
+
+def get_build_wall_sequence(wall_blueprint):
+    buid_wall_sequence = []
+    current_z = 0.0
+    for brick_row in wall_blueprint:
+        current_x = 0.0
+        build_row_sequence = []
+        for brick_color in brick_row:
+            brick_position = Point()
+            brick_position.x = current_x + 0.5 * brick_scales[brick_color].x
+            brick_position.y = 0.5 * brick_scales[brick_color].y
+            brick_position.z = current_z + 0.5 * brick_scales[brick_color].z
+            current_x += brick_scales[brick_color].x
+
+            brick_in_wall = BrickInWall(color = brick_color, position = brick_position)
+            build_row_sequence.append(brick_in_wall)
+
+        buid_wall_sequence.append(build_row_sequence)
+        current_z += brick_scales['red'].z  # As all bricks (should) have the same height
+    return buid_wall_sequence
+
+
+def main():
+    buid_wall_sequence = get_build_wall_sequence(wall_blueprint)
+    for i, row in enumerate(buid_wall_sequence):
+        for brick in row:
+            print('row[{}].pose = {}'.format(i, brick))
+
+if __name__ == '__main__':
+    main()
