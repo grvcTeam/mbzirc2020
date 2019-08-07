ad960639fbdd1850e1446a2ebc3a017e2f809054
Fran Real <fran.real.perez@gmail.com>
2019-08-07 17:55:44 +0200

    Clean up

--- ual_action_server/src/ual_action_server.cpp
+++ ual_action_server/src/ual_action_server.cpp
@@ -22,9 +22,7 @@
 #include <actionlib/server/simple_action_server.h>
 #include <uav_abstraction_layer/ual.h>
 #include <mbzirc_comm_objs/TakeOffAction.h>
-// #include <mbzirc_comm_objs/HoverAction.h>
 #include <mbzirc_comm_objs/GoToAction.h>
-// #include <mbzirc_comm_objs/FollowPathAction.h>
 #include <mbzirc_comm_objs/PickAction.h>
 #include <mbzirc_comm_objs/ObjectDetection.h>
 #include <mbzirc_comm_objs/ObjectDetectionList.h>
@@ -110,9 +108,7 @@ protected:
   ros::NodeHandle nh_;
   // NodeHandle instance must be created before this line. Otherwise strange error occurs
   actionlib::SimpleActionServer<mbzirc_comm_objs::TakeOffAction> take_off_server_;
-  // actionlib::SimpleActionServer<mbzirc_comm_objs::HoverAction> hover_server_;
   actionlib::SimpleActionServer<mbzirc_comm_objs::GoToAction> go_to_server_;
-  // actionlib::SimpleActionServer<mbzirc_comm_objs::FollowPathAction> follow_path_server_;
   actionlib::SimpleActionServer<mbzirc_comm_objs::PickAction> pick_server_;
   std::string robot_id_;  // TODO: Used?
   grvc::ual::UAL *ual_;
@@ -123,9 +119,7 @@ public:
 
   UalActionServer(std::string _robot_id):
     take_off_server_(nh_, "take_off_action", boost::bind(&UalActionServer::takeOffCallback, this, _1), false),
-    // hover_server_(nh_, "hover_action", boost::bind(&UalActionServer::hoverCallback, this, _1), false),
     go_to_server_(nh_, "go_to_action", boost::bind(&UalActionServer::goToCallback, this, _1), false),
-    // follow_path_server_(nh_, "follow_path_action", boost::bind(&UalActionServer::followPathCallback, this, _1), false),
     pick_server_(nh_, "pick_action", boost::bind(&UalActionServer::pickCallback, this, _1), false),
     robot_id_(_robot_id) {
 
@@ -133,9 +127,7 @@ public:
     // ros::param::set("~pose_frame_id", "map");
     ual_ = new grvc::ual::UAL();
     take_off_server_.start();
-    // hover_server_.start();
     go_to_server_.start();
-    // follow_path_server_.start();
     pick_server_.start();
   }
 
@@ -188,57 +180,6 @@ public:
         take_off_server_.setAborted(result);
     }
   }
-
-  /*
-  void hoverCallback(const mbzirc_comm_objs::HoverGoalConstPtr &_goal) {
-    // ROS_INFO("Hover!");
-    // mbzirc_comm_objs::HoverFeedback feedback;
-    mbzirc_comm_objs::HoverResult result;
-
-    while ((ual_->state().state == uav_abstraction_layer::State::UNINITIALIZED) && ros::ok()) {
-      ROS_WARN("UAL is uninitialized!");  // ROS_WARN("UAL %d is uninitialized!", uav_id);
-      sleep(1);
-    }
-    // TODO: Fill result
-    switch (ual_->state().state) {
-      case uav_abstraction_layer::State::LANDED_DISARMED:
-        ROS_WARN("UAL is disarmed!");
-        hover_server_.setAborted(result);
-        break;
-      case uav_abstraction_layer::State::LANDED_ARMED: {
-        // TODO: from param?
-        double current_z = ual_->pose().pose.position.z;
-        ual_->takeOff(_goal->height - current_z, true);  // TODO: timeout? preempt?
-        hover_server_.setSucceeded(result);
-        break;
-      }
-      case uav_abstraction_layer::State::TAKING_OFF:
-        ROS_WARN("UAL is taking off!");
-        // TODO: Wait until FLYING_AUTO?
-        hover_server_.setAborted(result);
-        break;
-      case uav_abstraction_layer::State::FLYING_AUTO: {
-        geometry_msgs::PoseStamped hover_pose = ual_->pose();
-        hover_pose.pose.position.z = _goal->height;
-        ual_->goToWaypoint(hover_pose, true);  // TODO: timeout? preempt?
-        hover_server_.setSucceeded(result);
-        break;
-      }
-      case uav_abstraction_layer::State::FLYING_MANUAL:
-        ROS_WARN("UAL is flying manual!");
-        hover_server_.setAborted(result);
-        break;
-      case uav_abstraction_layer::State::LANDING:
-        ROS_WARN("UAL is landing!");
-        // TODO: Wait until LANDED_ARMED and then take off?
-        hover_server_.setAborted(result);
-        break;
-      default:
-        ROS_ERROR("Unexpected UAL state!");
-        hover_server_.setAborted(result);
-    }
-  }
-*/
 
   void goToCallback(const mbzirc_comm_objs::GoToGoalConstPtr &_goal) {
     // ROS_INFO("Go to!");
@@ -421,9 +362,9 @@ public:
       velocity.twist.linear.z = z_pid.control_signal(target_position.z, 1.0 / CATCHING_LOOP_RATE);
 
       ual_->setVelocity(velocity);
-      ROS_INFO("Candidate relative position = [%lf, %lf, %lf]", matched_candidate_.relative_position.x, matched_candidate_.relative_position.y, matched_candidate_.relative_position.z);
-      ROS_INFO("target_position = [%lf, %lf, %lf] target angle = x", target_position.x, target_position.y, target_position.z);
-      ROS_INFO("velocity = [%lf, %lf, %lf]", velocity.twist.linear.x, velocity.twist.linear.y, velocity.twist.linear.z);
+      // ROS_INFO("Candidate relative position = [%lf, %lf, %lf]", matched_candidate_.relative_position.x, matched_candidate_.relative_position.y, matched_candidate_.relative_position.z);
+      // ROS_INFO("target_position = [%lf, %lf, %lf] target angle = x", target_position.x, target_position.y, target_position.z);
+      // ROS_INFO("velocity = [%lf, %lf, %lf]", velocity.twist.linear.x, velocity.twist.linear.y, velocity.twist.linear.z);
 
       // TODO: Look for equivalent!
       // if (catching_device_->switchIsPressed()) {
@@ -458,86 +399,6 @@ public:
     // ual_->goToWaypoint(_goal->waypoint);  // TODO: timeout? preempt?
     sensed_sub_.shutdown();
   }
-
-/*
-  void goToCallback(const mbzirc_comm_objs::GoToGoalConstPtr &_goal) {
-    ROS_INFO("Go to!");
-    // mbzirc_comm_objs::GoToFeedback feedback;
-    mbzirc_comm_objs::GoToResult result;
-    // TODO: Fill result
-
-    if (ual_->state().state != uav_abstraction_layer::State::FLYING_AUTO) {
-      ROS_WARN("UAL is not flying auto!");  // ROS_WARN("UAL %d is not flying auto!", uav_id);
-      go_to_server_.setAborted(result);
-    }
-
-    ual_->goToWaypoint(_goal->waypoint, true);  // TODO: blocking? preemptable?
-    go_to_server_.setSucceeded(result);
-  }
- */
-  /*
-  void followPathCallback(const mbzirc_comm_objs::FollowPathGoalConstPtr &_goal) {
-    // ROS_INFO("Follow path!");  // TODO: check for collisions?
-    // mbzirc_comm_objs::GoToFeedback feedback;
-    mbzirc_comm_objs::FollowPathResult result;
-    // TODO: Fill result
-
-    if (ual_->state().state != uav_abstraction_layer::State::FLYING_AUTO) {
-      ROS_WARN("UAL is not flying auto!");  // ROS_WARN("UAL %d is not flying auto!", uav_id);
-      follow_path_server_.setAborted(result);
-    }
-
-    // for (size_t i = 0; i < _goal->path.size(); i++) {
-    //   ual_->goToWaypoint(_goal->path[i], true);  // TODO: blocking? preemptable?
-    // }
-    for (auto waypoint: _goal->path) {
-      ual_->goToWaypoint(waypoint, true);  // TODO: blocking? preemptable?
-    }
-
-    follow_path_server_.setSucceeded(result);
-  }
-  */
-  /*
-    // helper variables
-    ros::Rate r(1);
-    bool success = true;
-
-    // push_back the seeds for the fibonacci sequence
-    feedback_.sequence.clear();
-    feedback_.sequence.push_back(0);
-    feedback_.sequence.push_back(1);
-
-    // publish info to the console for the user
-    ROS_INFO("%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i", action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
-
-    // start executing the action
-    for(int i=1; i<=goal->order; i++)
-    {
-      // check that preempt has not been requested by the client
-      if (as_.isPreemptRequested() || !ros::ok())
-      {
-        ROS_INFO("%s: Preempted", action_name_.c_str());
-        // set the action state to preempted
-        as_.setPreempted();
-        success = false;
-        break;
-      }
-      feedback_.sequence.push_back(feedback_.sequence[i] + feedback_.sequence[i-1]);
-      // publish the feedback
-      as_.publishFeedback(feedback_);
-      // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-      r.sleep();
-    }
-
-    if(success)
-    {
-      result_.sequence = feedback_.sequence;
-      ROS_INFO("%s: Succeeded", action_name_.c_str());
-      // set the action state to succeeded
-      as_.setSucceeded(result_);
-    }
-    */
-  // }
 };
 
 
@@ -548,7 +409,6 @@ int main(int argc, char** argv)
   UalActionServer ual_action_server("");  // Not needed?
   ros::MultiThreadedSpinner spinner(2);
   spinner.spin();
-  // ros::spin();
 
   return 0;
 }
