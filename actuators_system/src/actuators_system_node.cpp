a853ae739fd06c916ef9e05bcae6a00625da5806
Fran Real <fran.real.perez@gmail.com>
2020-01-07 16:19:28 +0100

    Add servers and pubishers to actuators node

--- actuators_system/src/actuators_system_node.cpp
+++ actuators_system/src/actuators_system_node.cpp
@@ -1,14 +1,63 @@
+#include <mutex>
+
 #include <ros/ros.h>
+#include <mbzirc_comm_objs/SetPWM.h>
+#include <mbzirc_comm_objs/SetDigital.h>
+#include <mbzirc_comm_objs/ActuatorsData.h>
 
 #include <actuators_system/serial_port.h>
 #include <actuators_system/arduino/ssfp.h>
 #include <actuators_system/arduino/board_input.h>
 #include <actuators_system/arduino/board_output.h>
 
+#define PWM_COUNT 5
+
 uint8_t rx_buffer[2*BOARD_OUTPUT_MIN_BUFFER_SIZE];
 uint8_t tx_buffer[BOARD_INPUT_MIN_BUFFER_SIZE];
 uint8_t aux_rx_buffer[BOARD_OUTPUT_MAX_MSG_SIZE];
 uint8_t aux_tx_buffer[BOARD_INPUT_MAX_MSG_SIZE];
+
+BoardInput board_input;
+std::mutex input_mutex;
+
+bool set_pwm(mbzirc_comm_objs::SetPWM::Request  &req, mbzirc_comm_objs::SetPWM::Response &res) {
+    if (req.id >= PWM_COUNT) {
+        ROS_ERROR("%s: PWM id [%d] out of bounds", __FUNCTION__, req.id);
+        return false;
+    }
+    // TODO: Check PWM is in range?
+
+    input_mutex.lock();
+    board_input.pwm[req.id] = req.pwm;
+    input_mutex.unlock();
+    return true;
+}
+
+bool set_digital(mbzirc_comm_objs::SetDigital::Request  &req, mbzirc_comm_objs::SetDigital::Response &res) {
+    if (req.id >= 1) {
+        ROS_ERROR("%s: Digital id [%d] out of bounds", __FUNCTION__, req.id);
+        return false;
+    }
+
+    input_mutex.lock();
+    board_input.digital_out_0 = req.value;
+    input_mutex.unlock();
+    return true;
+}
+
+mbzirc_comm_objs::ActuatorsData from_board_output(const BoardOutput& board_output) {
+    mbzirc_comm_objs::ActuatorsData actuators_data;
+    actuators_data.digital_reading[0] = board_output.digital_in_0;
+    actuators_data.digital_reading[1] = board_output.digital_in_1;
+    actuators_data.digital_reading[2] = board_output.digital_in_2;
+    actuators_data.digital_reading[3] = board_output.digital_in_3;
+    for (int i = 0; i < PWM_COUNT; i++) {
+        actuators_data.pwm_setpoint[i] = board_output.input_echo.pwm[i];
+    }
+    actuators_data.digital_setpoint = board_output.input_echo.digital_out_0;
+
+    return actuators_data;
+}
 
 class BoardOutputReader: public OnReadInterface {
 public:
@@ -27,7 +76,8 @@ public:
 };
 
 int main(int argc, char** argv) {
-   ros::init(argc, argv, "actuators_system_node");
+    ros::init(argc, argv, "actuators_system_node");
+    ros::NodeHandle n;
 
     // Open the serial port. Change device path as needed
     int serial_port = open("/dev/ttyUSB0", O_RDWR);
@@ -39,22 +89,17 @@ int main(int argc, char** argv) {
 
     serial_port::configure(serial_port);
     serial_port::lock(serial_port);
-    // usleep(5e5);
 
     Framer framer;
-    BoardInput board_input;
-    board_input.pwm[0] = 900;
-    board_input.pwm[1] = 901;
-    board_input.pwm[2] = 902;
-    board_input.pwm[3] = 903;
-    board_input.pwm[4] = 904;
-    board_input.digital_out_0 = false;
+    ros::ServiceServer set_pwm_service = n.advertiseService("set_pwm", set_pwm);
+    ros::ServiceServer set_digital_service = n.advertiseService("set_digital", set_digital);
 
     Deframer deframer;
     BoardOutputReader board_output_reader;
     deframer.connect(&board_output_reader);
+    ros::Publisher actuators_data_pub = n.advertise<mbzirc_comm_objs::ActuatorsData>("actuators_data", 10);
 
-    int i = 0;
+    ros::Rate rate(10);  // [Hz]
     while (ros::ok()) {
 
         size_t rx_bytes = read(serial_port, &rx_buffer, sizeof(rx_buffer));
@@ -73,8 +118,8 @@ int main(int argc, char** argv) {
 
         if (board_output_reader.has_new_data) {
             board_output_reader.has_new_data = false;
-            printf("\n________[%d]________\n\n", i);
-            board_output_reader.data.print();
+            actuators_data_pub.publish(from_board_output(board_output_reader.data));
+            // board_output_reader.data.print();
 
             if (board_output_reader.data.rx_error.crc) { printf("out::error: crc\n"); }
             if (board_output_reader.data.rx_error.lost) { printf("out::error: lost\n"); }
@@ -87,8 +132,8 @@ int main(int argc, char** argv) {
         size_t frame_size = framer.frame(aux_tx_buffer, msg_size, tx_buffer);
         write(serial_port, tx_buffer, frame_size);
 
-        usleep(1e5);
-        i++;
+        ros::spinOnce();
+        rate.sleep();
     }
 
     close(serial_port);
