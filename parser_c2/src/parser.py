94dc0421aa76f12665fb3dfc662281436301dd99
javidomjim <javidomjim95@gmail.com>
2020-02-10 13:02:24 +0100

    Adding parser with dictionaries

--- parser_c2/src/parser.py
+++ parser_c2/src/parser.py
@@ -1,139 +1,110 @@
 #!/usr/bin/env python
-
-from os import remove 
-import numpy as np
-import array 
 import roslaunch
 import rospy
-import sys
 
-# Global variables to obtain data from launcher
-global a
-global b
-global c
+# Class to read the brick order 
+class parser:
 
-# Routine to extract information from data given in txt
-def parser_request():
-    origin = open(a, 'r' )
-    # Creating an auxiliary file to extract information
-    port = open ('Puente_es.txt', 'w')
-    reading=(origin.readlines())
-    # Converting every word with a comma or a symbol into a simple string
-    for renglon in reading:
-        for palabra in renglon.split(' '):
-            dest=palabra.lower()
-            if palabra=="1," or palabra=="1:":
-                dest="1"
-            elif palabra=="2," or palabra=="2:":
-                dest="2"
-            elif palabra=="3," or palabra=="3:":
-                dest="3"
-            elif palabra=="4," or palabra=="4:":
-                dest="4"
-            elif palabra=="5," or palabra=="5:":
-                dest="5"
-            elif palabra=="6," or palabra=="6:":
-                dest="6"
-            elif palabra=="7," or palabra=="7:":
-                dest="7"
-            elif palabra=="8," or palabra=="8:":
-                dest="8"
-            elif palabra=="9," or palabra=="9:":
-                dest="9"
-            else:
-                port.write(" ")
-            port.write(dest)
-    port.close()
-
-    port = open ('Puente_es.txt','r')
-    fin = open(c,'w')
-    reading_port=(port.readlines())
-
-    for renglon in reading_port:
-        for palabra in renglon.split(' '):
-            # Translate UGV orders
-            if palabra=="segment1":
-                dest="S1"
-            elif palabra=="segment2":
-                dest="S2"
-            elif palabra=="segment3":
-                dest="S3"
-            elif palabra=="segment4":
-                dest="S4"
-        # Translate UAV orders
-            elif palabra=="channel1":
-                dest="C1"
-            elif palabra=="channel2":
-                dest="C2"
-            elif palabra=="channel3":
-                dest="C3"
-            elif palabra=="channel4":
-                dest="C4"
-            elif palabra=="channel5":
-                dest="C5"
-            elif palabra=="channel6":
-                dest="C6"
-        # Translate layers
-            elif palabra=="layer1":
-                dest=".1 " 
-            elif palabra=="layer2":
-                dest=".2 "
-            elif palabra=="layer3":
-                dest=".3 "
-            elif palabra=="layer4":
-                dest=".4 "
-            elif palabra=="layer5":
-                dest=".5 "
-            elif palabra=="layer6":
-                dest=".6 "
-            elif palabra=="layer7":
-                dest=".7 "
-            elif palabra==" ":
-                dest=" "
-            # Translate colours and endline colours 
-            elif palabra=="blue" or palabra=="blue\n":
-                dest="B "
-            elif palabra=="red" or palabra=="red\n":
-                dest="R "
-            elif palabra=="green" or palabra=="green\n":
-                dest="G "
-            elif palabra=="orange" or palabra=="orange\n":
-                dest="O "
-            else:
-                dest=""
-            fin.write(dest)
-        fin.write("\n")
-    fin.close()
-    #Eliminating auxiliary txt
-    remove('Puente_es.txt')
-
-    fin = open(c,'r')
-    reading_port=(fin.readlines())    
-    # Obtaining segment-layer or channel-layer to read and print
-    valor=b
-    with open (c,'r') as f:
-        l = [[str(num) for num in line.split(' ')] for line in f]
-
-    i=0
-    j=1
-    while i<len(l):
-        if l[i][j-1]==valor:
-            # Uncomment next line to print the complete array
-            # print(l[i])
-            while j!=len(l[i]):
-                print(l[i][j])
-                j=j+1
-            j=1
-        i=i+1            
-    
-    fin.close()
-            
-        
-            
-if __name__ == '__main__':
     rospy.init_node('parser',anonymous=True)
+    # Obtaining the file names through a launch file
+    ugv_file = rospy.get_param('~UGV_data')
+    uav_file = rospy.get_param('~UAV_data')
     
-    a = rospy.get_param('~File_data')
-    b = rospy.get_param('~Line_to_read')
-    c = rospy.get_param('~Extracted_data')
-    parser_request()
+    # Routine to read the UGV bricks
+    def UGV_parser_request(self):
+        origin = open(self.ugv_file, 'r' )
+        reading=(origin.readlines())
+
+        # Converting the data files into an array 
+        with open (self.ugv_file,'r') as f:
+            l = [[str(num) for num in line.split(' ')] for line in f]
+        
+        # Declaring a dictionary for the UGV
+        UGV_dict= dict()
+        i=0
+        j=0
+        # Read the number of rows with i 
+        while i<len(l):
+            j=0
+            # Read every column and delete \n from array if needed
+            while j!=len(l[i]):
+                if l[i][j]=="R\n":
+                    l[i][j]="R"
+                elif l[i][j]=="G\n":
+                    l[i][j]="G"
+                elif l[i][j]=="B\n":
+                    l[i][j]="B"
+                elif l[i][j]=="O\n":
+                    l[i][j]="O"
+                j=j+1       
+            # Obtaining KEY in dictionary, name of the layer of the current row
+            layer = 'L' + str((len(l)-i))
+            # Creating dictionary whit format: "LAYER : LIST OF COLOURS"
+            UGV_dict[str(layer)]=l[i]
+            i=i+1            
+        origin.close()
+        # Return dictionary 
+        return UGV_dict
+
+    # Routine to read UAV bricks
+    def UAV_parser_request(self):
+        origin = open(self.uav_file, 'r' )
+        reading=(origin.readlines())
+
+        # Converting the data files into an array 
+        with open (self.uav_file,'r') as f:
+            l = [[str(num) for num in line.split(' ')] for line in f]
+        
+        # Declaring a dictionary for the UAV 
+        UAV_dict= dict()
+        
+        segment_length=7  # Maximun lenght of segments
+        segment_number=3  # Maximun number of segments
+        seg = ['','','','','','',''] # Auxiliar list to store segment data of the layers
+        i=0
+        j=0
+        jmax=1  # index needed to separate segments in their respective layer
+        w=0 # Index needed to go through the auxiliary list
+        while i<len(l):
+            # Iteration to go through every segment
+            while jmax<segment_number+1:  
+                # Iteration to store every segment in a layer into a dictionary
+                while j<segment_length*jmax:
+                    if l[i][j]=="R\n":
+                        l[i][j]="R"
+                    elif l[i][j]=="G\n":
+                        l[i][j]="G"
+                    elif l[i][j]=="B\n":
+                        l[i][j]="B"
+                    elif l[i][j]=="O\n":
+                        l[i][j]="O"
+                    seg[w]=l[i][j]
+                    j=j+1
+                    w=w+1
+                    # Create KEY to the dictionary, composed by Layer + Segment
+                    segment = 'L' + str((len(l)-i)) + 'S' + str(jmax)
+                # Adding layer and segment to dictionary with format: "LayerXSegmentY : LIST OF COLOURS" 
+                UAV_dict[str(segment)]=str(seg)
+                w=0
+                jmax=jmax+1
+            i=i+1  
+            w=0
+            j=0    
+            jmax=1     
+        origin.close()
+        # Return UAV dictionary
+        return UAV_dict
+
+if __name__ == '__main__':
+
+    # Read parser class and obtain dictionaries
+    Read_data = parser()
+    UAV = Read_data.UAV_parser_request()
+    UGV = Read_data.UGV_parser_request()
+
+    # # Uncomment this lines to print in terminal
+    # for x, y in UAV.items():
+    #     print (x,y)
+
+    # for x, y in UGV.items():
+    #     print (x,y)
